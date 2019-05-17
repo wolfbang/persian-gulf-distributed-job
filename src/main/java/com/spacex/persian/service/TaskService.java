a713ed8788c2c40a71c8928feeb58a3e7c723d82
lucas ley <ayaaa168@163.com>
2019-05-17 23:09:05 +0800

    feat(Task):add task service

--- src/main/java/com/spacex/persian/service/TaskService.java
+++ src/main/java/com/spacex/persian/service/TaskService.java
@@ -4,11 +4,16 @@ import com.spacex.persian.dto.TaskDTO;
 import com.spacex.persian.dto.TaskUpdateDTO;
 import com.spacex.persian.enums.TaskStatusEnum;
 import com.spacex.persian.repository.mapper.TaskMapper;
+import com.spacex.persian.repository.po.TaskPO;
+import com.spacex.persian.util.BeanCopyUtil;
+import org.apache.commons.collections4.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class TaskService {
@@ -19,19 +24,55 @@ public class TaskService {
     private TaskMapper taskMapper;
 
     public TaskDTO getNextTask() {
-        return null;
+        Example example = new Example(TaskPO.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("status", TaskStatusEnum.WAITING.getCode());
+        example.setOrderByClause("id asc");
+        List<TaskPO> taskPOs = taskMapper.selectByExample(example);
+        if (CollectionUtils.isEmpty(taskPOs)) {
+            return null;
+        }
+        TaskPO taskPO = taskPOs.get(0);
+        TaskDTO taskDTO = BeanCopyUtil.map(taskPO, TaskDTO.class);
+        return taskDTO;
     }
 
     public TaskDTO getById(Long taskId) {
-        return null;
+        Example example = new Example(TaskPO.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("id", taskId);
+        example.setOrderByClause("id asc");
+        List<TaskPO> taskPOs = taskMapper.selectByExample(example);
+        if (CollectionUtils.isEmpty(taskPOs)) {
+            return null;
+        }
+        TaskPO taskPO = taskPOs.get(0);
+        TaskDTO taskDTO = BeanCopyUtil.map(taskPO, TaskDTO.class);
+        return taskDTO;
     }
 
     public TaskDTO update(TaskUpdateDTO taskUpdateDTO) {
-        return null;
+        Long taskId = taskUpdateDTO.getId();
+        Example example = new Example(TaskPO.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("id", taskId);
+        TaskPO taskPO = BeanCopyUtil.map(taskUpdateDTO, TaskPO.class);
+        taskMapper.updateByExampleSelective(taskPO, example);
+        return getById(taskId);
     }
 
     public TaskDTO updateStatus(Long taskId, TaskStatusEnum taskStatusEnum) {
-        return null;
+        if (taskId == null || taskStatusEnum == null) {
+            return null;
+        }
+
+        Example example = new Example(TaskPO.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("id", taskId);
+        TaskPO taskPO = new TaskPO();
+        taskPO.setTaskStatus(taskStatusEnum.getCode());
+        taskMapper.updateByExampleSelective(taskPO, example);
+        return getById(taskId);
     }
 
 }
